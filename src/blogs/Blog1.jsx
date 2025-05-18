3e144ac288bcdb6013e8e6f56126e32b92147b47
Jeffrey Lu <jefflu288@gmail.com>
2025-05-18 00:25:44 -0400

    edited blog1

--- src/blogs/Blog1.jsx
+++ src/blogs/Blog1.jsx
@@ -35,7 +35,7 @@ export default function Blog1() {
             learned a lot about those tools while building that webpage, but as it continued to grow bigger and bigger with all of the new projects
             I wanted to post on it along with more blogs, it became somewhat messy and difficult to maintain. This motivated me to launch a completely 
             new webpage that would be easier and cleaner to maintain and thus I decided to make it using <strong>React</strong> and <strong>Vite</strong>
-            for its fast build times and limited configuration hassles. 
+             for its fast build times and limited configuration hassles. 
           </p>
           <p>
             Nonetheless, I hope you enjoy exploring my new website, learning more about me and checking in on what I've been working on!
