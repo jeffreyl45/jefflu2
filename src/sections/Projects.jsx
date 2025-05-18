ad218695142569f2c44b101585be4f0d3f46ade5
Jeffrey Lu <jefflu288@gmail.com>
2025-05-17 23:20:14 -0400

    fixed issue with images not displaying in projects section on host

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,70 +1,70 @@
 import { useState } from 'react'
 import './Projects.css'
-const BASE = import.meta.env.BASE_URL
-
+const BASE = import.meta.env.BASE_URL;
 
 const projects = [
-   {
+  {
     title: 'Personal Portfolio Website',
     description: 'New Personal Portfolio website made using React Vite',
-    image: '/icon.png',
+    image: `${BASE}icon.png`,
     link: 'https://github.com/jeffreyl45/jefflu2'
-  },  
+  },
   {
     title: 'Old Personal Portfolio Website',
     description: "My old personal portfolio website made using HTML, CSS, JS",
-    image: '/personalPortfolio.png',
+    image: `${BASE}personalPortfolio.png`,
     link: 'https://github.com/jeffreyl45/jefflu'
   },
   {
     title: 'MultiChat',
     description: 'Mock Social Media Direct Messaging Full Stack Web Application in React and Python.',
-    image: '/multichatLogo.png',
+    image: `${BASE}multichatLogo.png`,
     link: 'https://github.com/jeffreyl45/multichat'
   },
   {
     title: 'Similar Image Search',
     description: "A machine learning application that searches for the top 6 visually similar images in an HNSW database to an input.",
-    image: '/hnsw.png',
+    image: `${BASE}hnsw.png`,
     link: 'https://github.com/jeffreyl45/Similar-Image-Search'
   },
   {
     title: 'tAI',
     description: 'A classroom website using Cohere API to provide customized chatboxes, offering instant and personalized assistance.',
-    image: '/tai.png',
+    image: `${BASE}tai.png`,
     link: 'https://github.com/deltahacks-tai/backend'
   },
   {
     title: 'TLDR Roger Ebert Reviews',
     description: "A machine learning project that summarizes an article from Roger Ebert's Website",
-    image: '/roger.jpg',
+    image: `${BASE}roger.jpg`,
     link: 'https://github.com/jeffreyl45/TLDR-Roger-Ebert-Reviews'
   },
   {
     title: 'DrumKit Keyboard',
     description: 'Play different drum sounds by pressing the keys on your keyboard!',
-    image: '/drumkit.png',
+    image: `${BASE}drumkit.png`,
     link: 'https://github.com/jeffreyl45/DrumKit-Keyboard'
   },
   {
     title: 'Hocus Focus',
     description: 'A program that notifies you when you are distracted based on facial tracking using OpenCV library and Python.',
-    image: '/hocusFocus.png',
+    image: `${BASE}hocusFocus.png`,
     link: 'https://github.com/jeffreyl45/HocusFocus'
   },
   {
     title: 'Hangman',
     description: 'Hangman Game Implemented in Java',
-    image: '/hangman.png',
+    image: `${BASE}hangman.png`,
     link: 'https://github.com/jeffreyl45/Java-Hangman'
   },
   {
     title: 'You Got Rocked!',
     description: 'Players take turns picking rocks, the player that takes the last rock is the loser! Made using Python.',
-    image: '/rocks.jpg',
+    image: `${BASE}rocks.jpg`,
     link: 'https://github.com/jeffreyl45/YouGotRocked'
   }
-]
+];
+
 
 export default function Projects() {
   const [expanded, setExpanded] = useState(false)
